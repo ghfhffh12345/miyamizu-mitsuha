3035e73e6104d6cffdd62d8d65970e244c9f2569
ghfhffh12345 <ghfhffh12345@outlook.kr>
2021-10-05 18:01:18 +0900

    ㅇㅇ

--- commands/message/play/play.js
+++ commands/message/play/play.js
@@ -12,20 +12,23 @@ module.exports = {
         let gameData = client.gameData.get(message.channel.id)
 
         commandArgs.shift()
-        if (!client.gameData.has(message.channel.id)) {
-            client.gameData.set(message.channel.id, {
-                players: [{ id: message.author.id, color: 'blue' }, { id: commandArgs[0].substring(3, commandArgs[0].length - 1), color: 'red' }],
-                games: [[], [], [], [], [], [], [], [], [], []]
-            })
-            gameData = client.gameData.get(message.channel.id)
-        } else {
-            if (gameData.players[0].id == message.author.id) {
-                gameData.games[parseInt(commandArgs[0], 10) - 1].push({ number: parseInt(commandArgs[1], 10), color: gameData.players[0].color })
-                gameData.players.push(gameData.players[0])
-                gameData.players.shift()
+
+        if (commandArgs[0] != 'end') {
+            if (!client.gameData.has(message.channel.id)) {
+                client.gameData.set(message.channel.id, {
+                    players: [{ id: message.author.id, color: 'blue' }, { id: commandArgs[0].substring(3, commandArgs[0].length - 1), color: 'red' }],
+                    games: [[], [], [], [], [], [], [], [], [], []]
+                })
+                gameData = client.gameData.get(message.channel.id)
+            } else {
+                if (gameData.players[0].id == message.author.id) {
+                    gameData.games[parseInt(commandArgs[0], 10) - 1].push({ number: parseInt(commandArgs[1], 10), color: gameData.players[0].color })
+                    gameData.players.push(gameData.players[0])
+                    gameData.players.shift()
+                }
             }
-        }
-        console.log(gameData.games)
-        message.reply(`<@${gameData.players[0].id}>\n` + gameRendering(gameData.games))
+            console.log(gameData.games)
+            message.reply(`<@${gameData.players[0].id}>\n` + gameRendering(gameData.games))
+        } else client.gameData.delete(message.channel.id)
     }
 }
