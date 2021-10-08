9450b3c889e43830bebdbd5c96e705ccaf082f3e
ghfhffh12345 <ghfhffh12345@outlook.kr>
2021-10-08 16:57:01 +0900

    dd

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,20 +10,15 @@ module.exports = {
         let command = client.commands.get(commandArgs[0])
             || client.commands.find(cmd => cmd.command.aliases && cmd.command.aliases.includes(commandArgs[0]))
 
-        if (!command) {
-            if (client.gameData.has(message.channel.id)) {
-                
-            }
-        } else {
-            if (command.number_of_elements) {
-                if (commandArgs.length < command.number_of_elements) return message.reply(command.data.description)
-            }
-    
-            try {
-                command.execute(message, client, commandArgs)
-            } catch (error) {
-                message.reply(`에러가 발생했어요! :( => ${error}`)
-            }
+        if (!command) return
+        if (command.number_of_elements) {
+            if (commandArgs.length < command.number_of_elements) return message.reply(command.data.description)
+        }
+
+        try {
+            command.execute(message, client, commandArgs)
+        } catch (error) {
+            message.reply(`에러가 발생했어요! :( => ${error}`)
         }
     }
 }
